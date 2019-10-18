20e4c23d9b52ee8842fa070cbff287869b870ca8
Nelson Nyland <nelsonnyland@gmail.com>
2019-10-18 10:35:01 -0700

    Added formatting and pointer changes

--- Search.py
+++ Search.py
@@ -1,6 +1,10 @@
 import os
 import errno
 
+FILE_NAME_LENGTH = 30
+COUNT_NAME_LENGTH = 4
+POINTER_LENGTH = 50
+
 def main():
     # read from console the keyword search
     print('WORD SEARCH 0.1')
@@ -11,7 +15,7 @@ def main():
     searchString = input('Search: ')
     search(directoryString, searchString.lower())
 
-def search(directoryString, searchString):
+def search(directoryString, searchStr):
     foundBool = False
     # search and output
     os.chdir(directoryString)
@@ -21,12 +25,11 @@ def search(directoryString, searchString):
                 with open(file, errors='ignore') as fileinput:
                     for count, line in enumerate(fileinput):
                         lineStr = line.lower()
-                        if searchString in lineStr:
+                        pointer = '^'
+                        if searchStr in lineStr:
                             foundBool = True
-                            basename = os.path.basename(file)
-                            if len(basename) > 35:
-                                basename = basename[0:34] + '...'
-                            print('File {}: Line {}: {}'.format(basename, count + 1, line))
+                            set = StringSet(searchStr, os.path.basename(file), count, line, pointer)
+                            printToConsole(processStrings(set))
             except IOError as e:
                 if e.errno == errno.ENOENT:
                     print(os.path.basename(file), ' does not exist')
@@ -37,5 +40,36 @@ def search(directoryString, searchString):
     if foundBool is False:
         print('**NO WORDS FOUND**')
 
-main()
+# processes the strings to format in console
+def processStrings(set):
+    if len(set.basename) > FILE_NAME_LENGTH:
+        set.basename = set.basename[0:FILE_NAME_LENGTH - 3] + '...'
+    else:
+        set.basename = set.basename.ljust(FILE_NAME_LENGTH)
+    set.count += 1
+    set.count = str(set.count)
+    if len(set.count) > COUNT_NAME_LENGTH:
+        set.count = set.count[0:COUNT_NAME_LENGTH - 1] + '~'
+    else:
+        set.count = set.count.ljust(COUNT_NAME_LENGTH)
+    set.line = set.line.rstrip('\n')    
+    index = set.line.find(set.searchStr)    
+    set.pointer = set.pointer.rjust(index + POINTER_LENGTH)
+    return set
 
+# print to console
+def printToConsole(set):
+    print('FILE: {} LINE {}: \"{}\"'.format(set.basename, set.count, set.line))
+    if (len(set.pointer) > POINTER_LENGTH):
+        print(set.pointer)
+
+# boxes up the strings
+class StringSet:
+    def __init__(self, searchStr, basename, count, line, pointer):
+        self.searchStr = searchStr
+        self.basename = basename
+        self.count = count
+        self.line = line
+        self.pointer = pointer
+
+main()
