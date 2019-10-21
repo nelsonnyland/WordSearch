26167a1a1d5f5c3de234f7efc542ade80c163371
Nelson Nyland <nelsonnyland@gmail.com>
2019-10-21 13:11:23 -0700

    Pathing and formatting improvements

--- Search.py
+++ Search.py
@@ -2,38 +2,43 @@ import os
 import errno
 import glob
 import sys
+import pathlib
 
 FILE_NAME_LENGTH = 30
 COUNT_NAME_LENGTH = 4
-POINTER_LENGTH = 50
+POINTER_LENGTH = 55
 
 def main():
     intro()
     # read from console
-    directoryString = input('Dir: ')
+    path = input('Dir: ')
     # validate dir
-    dirValidate(directoryString)
+    dirValidate(path)
     # receive search term
-    searchStr = input('Search: ')
+    words = input('Search: ')
     # test search mode
-    mode = testMode(searchStr)
+    mode = testMode(words)
     # begin operation
-    openDir(directoryString, searchStr, mode)
+    openDir(path, words, mode)
+    # restart
     main()
 
 # prints intro text
 def intro():
     print('WORD SEARCH 0.1')
-    print('Searches TXT files within given directory and subdirectories. Use quotes to clarify search term.')
+    print('Searches TXT files within given directory and subdirectories. ' + 
+          'Use quotes to clarify search term.')
     print('Enter \'q\' to exit.')
 
 # validates directory
-def dirValidate(directoryString):
+def dirValidate(path):
     # check for exit command
-    if directoryString.lower() == 'q':
+    if path.lower() == 'q':
         sys.exit()
+    # correct backslashes
+    path.replace('\\', '/')
     # check if directory exists
-    if not os.path.exists(directoryString):
+    if not os.path.exists(path):
         print("must be a working directory\n")
         main()
 
@@ -46,69 +51,69 @@ def testMode(s):
         return 0
 
 # opens and searches files
-def openDir(directoryString, searchStr, mode):
+def openDir(path, words, mode):
     foundBool = False
     # search and output
-    os.chdir(directoryString)
-    for file in glob.iglob(directoryString + '/**/*.txt', recursive=True):
+    os.chdir(path)
+    for file in glob.iglob(path + '/**/*.txt', recursive=True):
         if os.path.isfile(file):
             try:
                 with open(file, errors='ignore') as fileinput:
                     for count, line in enumerate(fileinput):
-                        set = StringSet(mode, searchStr, os.path.basename(file), count, line)
+                        set = StringSet(mode, words, file, count, line)
                         if search(set):
                             foundBool = True
-                            printToConsole(processStrings(set))
+                            printToConsole(formatStrings(set))
             except IOError as e:
                 if e.errno == errno.ENOENT:
-                    print(os.path.basename(file), ' does not exist')
+                    print(os.path(file), ' does not exist')
                 if e.errno == errno.EACCES:
-                    print(os.path.basename(file), ' cannot be read')
+                    print(os.path(file), ' cannot be read')
                 else:
-                    print(os.path.basename(file), e)
+                    print(os.path(file), e)
     if foundBool is False:
         print('**NO WORDS FOUND**')
 
 def search(set):
     if set.mode is 0:
-        if set.searchStr in set.line:
+        if set.words in set.line:
             return True
     elif set.mode is 1:
-        set.searchStr = set.searchStr.strip('\"')
-        set.searchStr = set.searchStr.strip('\'')
-        newSearchStr = ' ' + set.searchStr + ' '
-        if newSearchStr in set.line:
+        set.words = set.words.strip('\"')
+        set.words = set.words.strip('\'')
+        newSearch = ' ' + set.words + ' '
+        if newSearch in set.line:
             return True
 
 # processes the strings to format in console
-def processStrings(set):
-    if len(set.basename) > FILE_NAME_LENGTH:
-        set.basename = set.basename[0:FILE_NAME_LENGTH - 3] + '...'
-    else:
-        set.basename = set.basename.ljust(FILE_NAME_LENGTH)
+def formatStrings(set):    
+    if len(set.path) > FILE_NAME_LENGTH:
+        set.path = '...' + set.path[len(set.path) - 
+                    FILE_NAME_LENGTH:len(set.path)] + ' - '
     set.count += 1
-    set.count = str(set.count)
+    set.count = str(set.count) + ':'
     if len(set.count) > COUNT_NAME_LENGTH:
+        set.count = set.count.rstrip(':')
         set.count = set.count[0:COUNT_NAME_LENGTH - 1] + '~'
     else:
         set.count = set.count.ljust(COUNT_NAME_LENGTH)
     set.line = set.line.rstrip('\n')    
-    index = set.line.find(set.searchStr)    
+    index = set.line.find(set.words)    
     set.pointer = set.pointer.rjust(index + POINTER_LENGTH)
     return set
 
 # print to console
 def printToConsole(set):
-    print('FILE: {} LINE {}: \"{}\"'.format(set.basename, set.count, set.line))
+    print('FILE: {} LINE {} \"{}\"'.format(set.path, set.count, set.line))
     if len(set.pointer) > POINTER_LENGTH:
         print(set.pointer)
 
 # boxes up the strings
 class StringSet:
-    def __init__(self, mode, searchStr, basename, count, line):
+    def __init__(self, mode, words, path, count, line):
         self.mode = mode
-        self.searchStr = searchStr
-        self.basename = basename
+        self.words = words
+        self.path = path
         self.count = count
         self.line = line.lower()
         self.pointer = '^'
